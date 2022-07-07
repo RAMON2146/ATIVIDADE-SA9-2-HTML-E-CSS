eb9929807355a3bdfb1b48b0c30a7da74b719fa2
RAMON2146 <ramonssmdanilo2146@gmail.com>
2022-07-07 17:02:04 -0300

    4 commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function ligar(){
+    document.getElementById("ligar").innerHTML=alert("71 987613261");
+}
+
+function enviar(){
+    document.getElementById("enviar").innerHTML=alert("ENVIE PARA: ramonssmdanilo@yahoo.com.br");
+}
+
+function chamar(){
+    document.getElementById("chamar").innerHTML=alert("ADD: 71987613261");
+}
